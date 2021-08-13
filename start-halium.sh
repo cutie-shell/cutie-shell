3c26072ff60ca06b89c231c3c516984f4fcd8f61
Erik Inkinen <erik.inkinen@gmail.com>
2021-08-13 18:56:47 +0300

    Fix Keyboard size

--- start-halium.sh
+++ start-halium.sh
@@ -7,17 +7,5 @@ export QT_IM_MODULE=qtvirtualkeyboard
 export QT_VIRTUALKEYBOARD_LAYOUT_PATH=./layouts/
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export QT_SCALE_FACTOR="4"
-#export QT_QAYLAND_CLIENT_BUFFER_INTEGRATION=wayland-egl
-#export QT_QPA_EGLFS_KMS_CONFIG=/opt/cutiepi-shell/kms.conf
-#export XDG_RUNTIME_DIR=$HOME/.xdg
-
-#if [ ! "`systemctl is-active connman`" == "active" ]; then 
-#    sudo service connman start 
-#fi
-
-#rfkill unblock all
-#connmanctl disable wifi
-#connmanctl enable wifi 
-
-#/opt/qt5/bin/qmlscene /opt/cutiepi-shell/compositor.qml
+export QT_VIRTUALKEYBOARD_STYLE=cutie
 ./cutie-ui-io -plugin libinput
