d60ae61cb632a1c24b87d56497bfe66acd1799a1
Erik Inkinen <erik.inkinen@gmail.com>
2021-08-22 13:24:15 +0300

    Switch to maliit

--- start-halium.sh
+++ start-halium.sh
@@ -3,9 +3,8 @@
 export LD_LIBRARY_PATH="/usr/lib/aarch64-linux-gnu/libhybris:/vendor/lib64:/system/lib64:/usr/lib"
 export QT_QPA_PLATFORM=hwcomposer
 export EGL_PLATFORM=hwcomposer
-export QT_IM_MODULE=qtvirtualkeyboard
-export QT_VIRTUALKEYBOARD_LAYOUT_PATH=./layouts/
+export QT_IM_MODULE=Maliit
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export QT_SCALE_FACTOR="4"
-export QT_VIRTUALKEYBOARD_STYLE=cutie
+export MALIIT_FORCE_DBUS_CONNECTION=1
 ./cutie-ui-io -plugin libinput
