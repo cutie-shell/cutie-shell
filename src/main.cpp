99b9a959ecf1434866e7e5ccad9783512de18f11
Alexander Rutz <alex@familyrutz.com>
2023-03-10 18:10:54 -0700

    [VirtualKeyboard] Update import path for custom keyboard style. qt6-virtualkeyboard also needs this patch: https://github.com/cutie-shell/qt6-virtualkeyboard/commit/8214204b6c8e3661a4d89d39b22075425434b368

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ int main(int argc, char *argv[])
     Settings *settings = new Settings(&engine);
     settings->refreshBatteryInfo();
 
-    engine.addImportPath("qrc://kbdstyle");
+    engine.addImportPath("qrc:/kbdstyle");
     qputenv("QT_VIRTUALKEYBOARD_STYLE", "cutie");
 
     engine.rootContext()->setContextProperty("shellScaleFactor", shellScaleFactor);
